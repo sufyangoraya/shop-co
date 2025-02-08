51a94778d9e4d8c4aae501a35c0142de13d6f742
sufyangoraya <sufyangoraya913@gmail.com>
2025-02-08 16:41:21 +0500

    minnor  update

--- types/shipengine.ts
+++ types/shipengine.ts
@@ -1,66 +1,66 @@
-export interface ShippingAddress {
-    name: string
-    phone: string
-    addressLine1: string
-    addressLine2?: string
-    cityLocality: string
-    stateProvince: string
-    postalCode: string
-    countryCode: string
-    addressResidentialIndicator: "yes" | "no"
-  }
+// export interface ShippingAddress {
+//     name: string
+//     phone: string
+//     addressLine1: string
+//     addressLine2?: string
+//     cityLocality: string
+//     stateProvince: string
+//     postalCode: string
+//     countryCode: string
+//     addressResidentialIndicator: "yes" | "no"
+//   }
   
-  export interface PackageDetails {
-    weight: {
-      value: number
-      unit: "pound" | "ounce" | "gram" | "kilogram"
-    }
-    dimensions: {
-      length: number
-      width: number
-      height: number
-      unit: "inch" | "centimeter"
-    }
-  }
+//   export interface PackageDetails {
+//     weight: {
+//       value: number
+//       unit: "pound" | "ounce" | "gram" | "kilogram"
+//     }
+//     dimensions: {
+//       length: number
+//       width: number
+//       height: number
+//       unit: "inch" | "centimeter"
+//     }
+//   }
   
-  export interface ShippingRate {
-    rateId: string
-    rateType: string
-    carrierId: string
-    shippingAmount: {
-      currency: string
-      amount: number
-    }
-    insuranceAmount: {
-      currency: string
-      amount: number
-    }
-    confirmationAmount: {
-      currency: string
-      amount: number
-    }
-    otherAmount: {
-      currency: string
-      amount: number
-    }
-    taxAmount: {
-      currency: string
-      amount: number
-    }
-    zone: number
-    packageType: string
-    deliveryDays: number
-    guaranteedService: boolean
-    estimatedDeliveryDate: string
-    carrierDeliveryDays: string
-    shipDate: string
-    negotiatedRate: boolean
-    serviceType: string
-    serviceCode: string
-    trackable: boolean
-    carrierCode: string
-    carrierNickname: string
-    carrierFriendlyName: string
-  }
+//   export interface ShippingRate {
+//     rateId: string
+//     rateType: string
+//     carrierId: string
+//     shippingAmount: {
+//       currency: string
+//       amount: number
+//     }
+//     insuranceAmount: {
+//       currency: string
+//       amount: number
+//     }
+//     confirmationAmount: {
+//       currency: string
+//       amount: number
+//     }
+//     otherAmount: {
+//       currency: string
+//       amount: number
+//     }
+//     taxAmount: {
+//       currency: string
+//       amount: number
+//     }
+//     zone: number
+//     packageType: string
+//     deliveryDays: number
+//     guaranteedService: boolean
+//     estimatedDeliveryDate: string
+//     carrierDeliveryDays: string
+//     shipDate: string
+//     negotiatedRate: boolean
+//     serviceType: string
+//     serviceCode: string
+//     trackable: boolean
+//     carrierCode: string
+//     carrierNickname: string
+//     carrierFriendlyName: string
+//   }
   
   
--- types/shipping.ts
+++ types/shipping.ts
@@ -1,89 +1,89 @@
-export interface ShippingAddress {
-  name: string
-  phone: string
-  addressLine1: string
-  addressLine2?: string
-  cityLocality: string
-  stateProvince: string
-  postalCode: string
-  countryCode: string
-  addressResidentialIndicator: "yes" | "no"
-}
+// export interface ShippingAddress {
+//   name: string
+//   phone: string
+//   addressLine1: string
+//   addressLine2?: string
+//   cityLocality: string
+//   stateProvince: string
+//   postalCode: string
+//   countryCode: string
+//   addressResidentialIndicator: "yes" | "no"
+// }
 
-export interface PackageDetails {
-  weight: {
-    value: number
-    unit: "pound" | "ounce" | "gram" | "kilogram"
-  }
-  dimensions: {
-    height: number
-    width: number
-    length: number
-    unit: "inch" | "centimeter"
-  }
-}
+// export interface PackageDetails {
+//   weight: {
+//     value: number
+//     unit: "pound" | "ounce" | "gram" | "kilogram"
+//   }
+//   dimensions: {
+//     height: number
+//     width: number
+//     length: number
+//     unit: "inch" | "centimeter"
+//   }
+// }
 
-export interface ShippingRate {
-  rateId: string
-  carrierId: string
-  serviceCode: string
-  serviceName: string
-  deliveryDays: number
-  shipmentAmount: number
-  insuranceAmount: number
-  confirmationAmount: number
-  otherAmount: number
-  totalAmount: number
-}
+// export interface ShippingRate {
+//   rateId: string
+//   carrierId: string
+//   serviceCode: string
+//   serviceName: string
+//   deliveryDays: number
+//   shipmentAmount: number
+//   insuranceAmount: number
+//   confirmationAmount: number
+//   otherAmount: number
+//   totalAmount: number
+// }
 
-export interface ShippingDetails {
-  rate: ShippingRate
-  package: PackageDetails
-  estimatedDelivery: {
-    earliest: string
-    latest: string
-  }
-}
+// export interface ShippingDetails {
+//   rate: ShippingRate
+//   package: PackageDetails
+//   estimatedDelivery: {
+//     earliest: string
+//     latest: string
+//   }
+// }
 
-export interface LabelDownload {
-  href: string
-  pdf: string
-  png: string
-  zpl: string
-}
+// export interface LabelDownload {
+//   href: string
+//   pdf: string
+//   png: string
+//   zpl: string
+// }
 
-export interface TrackingEvent {
+// export interface TrackingEvent {
 
-  occurred_at: string
+//   occurred_at: string
 
-  carrier_occurred_at?: string
+//   carrier_occurred_at?: string
 
-  description?: string
+//   description?: string
 
-  city_locality?: string
+//   city_locality?: string
 
-  state_province?: string
+//   state_province?: string
 
-  postal_code?: string
+//   postal_code?: string
 
-  country_code?: string | null
+//   country_code?: string | null
 
-  company_name?: string | null
+//   company_name?: string | null
 
-  signer?: string | null
+//   signer?: string | null
 
-  event_code?: string | null
+//   event_code?: string | null
 
-}
+// }
 
 
 
-export interface TrackingStatus {
-  status: string
-  statusDescription: string
-  carrierStatusCode?: string
-  carrierDetailCode?: string
-  lastUpdate: string
-  events: TrackingEvent[]
-}
+// export interface TrackingStatus {
+//   status: string
+//   statusDescription: string
+//   carrierStatusCode?: string
+//   carrierDetailCode?: string
+//   lastUpdate: string
+//   events: TrackingEvent[]
+// }
 
